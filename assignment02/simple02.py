ff21e8a7d1909f26ad8514002f90f066e2b822fd
halomafac <153785368+halomafac@users.noreply.github.com>
2025-06-18 12:13:20 +0700

    HW

--- assignment02/simple02.py
+++ assignment02/simple02.py
@@ -1,2 +1,22 @@
 # Synchronous cooking
-# 2 kitchen 2 chefs 2 dishes
+# 1 kitchen 1 chefs 1 dish
+from time import sleep, ctime, time
+
+# Cooking synchronous
+def cooking(index):
+    print(f'{index}: Kitchen-{index} : Begin cooking...')
+    sleep(2)
+    print(f'{index}: Kitchen-{index} : Cooking done!')
+
+if __name__ == '__main__':
+    # Begin of main thread
+    print(f'{ctime} Main  : Start Cooking.')
+    start_time = time()
+    
+    # Cooking
+    for index in range(2):
+        cooking(index)
+    
+    duration = time() - start_time
+    
+    print(f'{ctime} Main  : Finished Cooking duration {duration:0.2f} seconds')
