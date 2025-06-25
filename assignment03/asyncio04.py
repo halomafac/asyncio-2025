5134379b798dd4a3d23ef135fcc573a2d4b33f29
halomafac <153785368+halomafac@users.noreply.github.com>
2025-06-25 10:21:44 +0700

    Asyncio1-4

--- assignment03/asyncio04.py
+++ assignment03/asyncio04.py
@@ -1,1 +1,16 @@
 # Starting task
+# Event Loop
+import asyncio
+
+async def greet():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World")
+
+asyncio.run(greet())  # ← สร้างและรัน event loop
+
+# แบบ manual event loop (commented)
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+# loop.run_until_complete(greet())
+# loop.close()
