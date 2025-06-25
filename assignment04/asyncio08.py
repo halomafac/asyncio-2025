29da68f0f103dd897ac13554346ecccd6deac125
halomafac <153785368+halomafac@users.noreply.github.com>
2025-06-25 11:45:44 +0700

    12.00

--- assignment04/asyncio08.py
+++ assignment04/asyncio08.py
@@ -1,1 +1,16 @@
 # get Exception
+import asyncio
+
+async def error_task():
+    await asyncio.sleep(1)
+    raise ValueError("เกิดข้อผิดหลาด")
+
+async def main():
+    task = asyncio.create_task(error_task())
+    try:
+        await task
+
+    except Exception:
+        print("Exception ที่เกิด:", task.exception())
+        
+asyncio.run(main())
