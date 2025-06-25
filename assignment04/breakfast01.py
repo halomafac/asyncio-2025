bca86c4e95b2e6ec81a0c6f564464b9cd1a6daa7
halomafac <153785368+halomafac@users.noreply.github.com>
2025-06-25 11:51:53 +0700

    breakfast

--- assignment04/breakfast01.py
+++ assignment04/breakfast01.py
@@ -19,7 +19,7 @@ def main():
     start = time()
     make_coffee()
     fry_eggs()
-    print(f"breakfast is ready in {time()-start} seconds.")
+    print(f"breakfast is ready in {time()-start} min")
 
 
 main()
--- assignment04/breakfast03.py
+++ assignment04/breakfast03.py
@@ -1,3 +1,31 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee():
+    print("coffee: prepare ingridents")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5)  # async sleep
+    print("coffee: ready")
+
+async def fry_eggs():
+    print("eggs: prepare ingridents")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3)  # async sleep
+    print("eggs: ready")
+
+async def main():
+    start = time()
+
+    # เริ่มทั้งสอง task พร้อมกัน
+    task1 = asyncio.create_task(make_coffee())
+    task2 = asyncio.create_task(fry_eggs())
+
+    await task1
+    await task2
+
+    print(f"breakfast is ready in {time() - start} min")
+
+asyncio.run(main())  # run top-level async function
