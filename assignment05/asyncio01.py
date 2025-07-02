6481d11da16d3cc8e0a1050362ba1326d6fba019
halomafac <153785368+halomafac@users.noreply.github.com>
2025-07-02 10:47:39 +0700

    AS05

--- assignment05/asyncio01.py
+++ assignment05/asyncio01.py
@@ -1,1 +1,25 @@
 # example of waiting for all tasks to complete
+from random import random
+import asyncio
+
+# coroutine to execute in new task
+async def task_coro(arg):
+    # generate a random value between 0 and 1
+    value = random()
+    # block for a moment
+    await asyncio.sleep(value)
+    # report the value
+    print(f'>task {arg} done with {value}')
+
+# main courutine
+async def main():
+    # create many tassks
+    tasks = [asyncio.create_task(task_coro(i)) for i in range (10)]
+    # wait for all tasks to complete
+    # done,pending = await asyncio.wait(tasks,return_when = asyncio.ALL_COMPLETED)
+    done,pending = await asyncio.wait(tasks)
+    # report results
+    print('All odne')
+
+# start the sysncio program
+asyncio.run(main())
--- assignment05/asyncio04.py
+++ assignment05/asyncio04.py
@@ -1,1 +1,25 @@
 # example of waiting for all tasks to be completed with a timeout
+from random import random
+import asyncio
+
+# coroutine to excute in new task
+async def task_coro(arg):
+    # generate a random value between 0 and 1
+    value = random()
+    # block for a moment
+    await asyncio.sleep(value)
+    # report the value
+    print(f'>task {arg} done with {value}')
+    return f"task {arg} with {value}"
+
+# main coroutine
+async def main():
+    # create many tasks
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    # wait for all take to complete
+    done, pending = await asyncio.wait(tasks, timeout=0.5)
+    # report results
+    print(f'Done, {len(done)} tasks completed in time')
+
+# start the asyncio program
+asyncio.run(main())
